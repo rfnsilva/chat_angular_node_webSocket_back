aa6a0839af1b5deafc920e1977f80a2960fd20ed
ricardotcu10 <ricardotcu10@gmail.com>
2020-08-05 05:26:05 -0300

    ajuste 16

--- dist/migration/1596609491913-insertgrupo.js
+++ dist/migration/1596609491913-insertgrupo.js
@@ -6,13 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.insertgrupo1596609491913 = void 0;
 
 class insertgrupo1596609491913 {
-  async up(queryRunner) {
-    await queryRunner.query(`INSERT INTO "grupo" ("nome") VALUES ('grupo 1')`);
-    await queryRunner.query(`INSERT INTO "grupo" ("nome") VALUES ('grupo 2')`);
-    await queryRunner.query(`INSERT INTO "grupo" ("nome") VALUES ('grupo 3')`);
-    await queryRunner.query(`INSERT INTO "grupo" ("nome") VALUES ('grupo 4')`);
-    await queryRunner.query(`INSERT INTO "grupo" ("nome") VALUES ('grupo 5')`);
-  }
+  async up(queryRunner) {}
 
   async down(queryRunner) {}
 
